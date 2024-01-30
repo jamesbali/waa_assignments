fb5e37dabd050a61b18998783fbd39461d979fc8
jamesbali <jamesbalimunsi@gmail.com>
2024-01-29 23:03:40 -0600

    lab 6

--- WebAppArch_Lab3/src/main/java/edu/miu/WebAppArchLab/domain/AspectOne.java
+++ WebAppArch_Lab3/src/main/java/edu/miu/WebAppArchLab/domain/AspectOne.java
@@ -1,2 +1,23 @@
-package edu.miu.WebAppArchLab.domain;public class AspectOne {
+package edu.miu.WebAppArchLab.domain;
+
+
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class AspectOne {
+
+    @After("@annotation(LogMethod)")
+
+    public void logMethod(Joinpo) {
+
+    }
+
+
+
+
+
 }
